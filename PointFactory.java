b6490b0989305ae828dd24aec3dfb613eb1bd060
SeanSweeney04 <115557622+SeanSweeney04@users.noreply.github.com>
2024-02-11 15:21:52 +0000

    Assignment 2 complete

--- PointFactory.java
+++ PointFactory.java
@@ -40,4 +40,37 @@ class Point implements GridQuadrant, CompareQuadrant {
         if (x > 0 && y < 0 && newX > 0 && newY < 0) {
             return true;
         }
-        if (x < 0 && y > 0 && newX < 0 && n
+        if (x < 0 && y > 0 && newX < 0 && newY > 0) {
+            return true;
+        }
+        if (x < 0 && y < 0 && newX < 0 && newY < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+enum Quadrant {
+    Q1, // x +ve, y +ve
+    Q2, // x -ve, y +ve
+    Q3, // x -ve, y -ve
+    Q4; // x +ve, y -ve
+}
+
+interface GridQuadrant {
+    Quadrant getQuadrant(); // return which quadrant the point is in
+    // if point is at (0,0) return null
+}
+
+interface CompareQuadrant {
+    boolean isInSameQuadrant(Point point); // true if the given point is
+    // in the same quadrant as this point
+}
+
+interface PointMaker {
+    Point makePoint(double x, double y); // returns a point with given x,y
+    int countPointsCreated(); // returns count of points created via factory
+}
+
+// TODO: create class PointFactory which implements the interface PointMaker
